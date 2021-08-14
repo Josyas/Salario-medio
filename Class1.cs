aae888b1de6810b6d59bd0ca4d989529d50a1fc7
Josias <87546094+Josyas@users.noreply.github.com>
2021-08-13 21:51:38 -0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex_02_OrientacaoObejtc
+{
+    class Funcionario{
+
+        public string Nome;
+        public double Salario;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex_02_OrientacaoObejtc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1 = new Funcionario();
+            Funcionario f2 = new Funcionario();
+            double media;
+
+            Console.WriteLine("Dados do primeiro funcionário: ");
+            Console.Write("Nome: ");
+            f1.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f1.Salario = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do segundo funcionário: ");
+            Console.Write("Nome: ");
+            f2.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f2.Salario = double.Parse(Console.ReadLine());
+
+            media = (f1.Salario + f2.Salario) / 2;
+            Console.WriteLine($"Salário média = {media.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
